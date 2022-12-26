6f3a4a87265f4d9dcf7e5f3fd3f8fea46375e513
ILGIZ <ilgizalimoff@yandex.ru>
2022-12-26 23:01:37 +0400

    1st update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ let sum = document.getElementById('sum')
 let mens = document.getElementById('mens')
 let procent = document.getElementById('procent')
 let calculate = document.getElementById('calculate')
+let container = document.querySelector('.container')
 
-calculate.addEventListener('click', function(){
+calculate.addEventListener('click', function () {
     let itog = 0
-    itog = (sum.value*(procent.value/100))/mens.value
-    console.log(itog);
+    itog = (sum.value * (procent.value / 100)) / mens.value
+
+    document.querySelector('.answerBlockWrapper').remove()
+    
+    let div = document.createElement('div')
+    div.className = 'answerBlockWrapper'
+    div.innerHTML = `<div class='answerBlock'> 
+                                 Чаевые на одного человека составляют ${itog.toFixed(2)}$
+                            </div>`
+    container.appendChild(div)
 })
