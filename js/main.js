b0872a724fc422ad2cc9e7ac3deaa29adb1afa93
ILGIZ <ilgizalimoff@yandex.ru>
2023-01-02 22:39:08 +0400

    fixed output

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ let calculate = document.getElementById('calculate')
 let container = document.querySelector('.container')
 
 calculate.addEventListener('click', function () {
-    let itog = 0
-    itog = (sum.value * (procent.value / 100)) / mens.value
-
-    document.querySelector('.answerBlockWrapper').remove()
-    
     let div = document.createElement('div')
     div.className = 'answerBlockWrapper'
-    div.innerHTML = `<div class='answerBlock'> 
-                                 Чаевые на одного человека составляют ${itog.toFixed(2)}$
-                            </div>`
-    container.appendChild(div)
+
+    if (sum.value == '' || mens.value == '' || procent.value == '') {
+        document.querySelector('.answerBlockWrapper').remove()
+        div.innerHTML = `<div class='answerBlock'> 
+            Заполните все поля!!!
+        </div>`
+        container.appendChild(div)
+
+    } else {
+        let itog = 0
+        itog = (sum.value * (procent.value / 100)) / mens.value
+        document.querySelector('.answerBlockWrapper').remove()
+        div.innerHTML = `<div class='answerBlock'> 
+            Чаевые на одного человека составляют ${itog.toFixed(2)}$
+         </div>`
+        container.appendChild(div)
+    }
+
 })
